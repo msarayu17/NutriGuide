29e0c4d0f4fd751413af33258c5ba7e3b2409577
Sarayu <90026579+msarayu17@users.noreply.github.com>
2026-07-30 20:13:49 +0530

    Rename home route to index and update path

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+@app.route("/index")
+def index():
     return render_template("index.html")  
 
 @app.route("/about")
